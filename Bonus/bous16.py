00db8e9b67bb7c9a17ccf776f76757b020cc21d2
Saaransh27 <133195191+Saaransh27@users.noreply.github.com>
2024-06-09 00:01:40 +0530

    completed the exit and complete buttons

--- Bonus/bous16.py
+++ Bonus/bous16.py
@@ -1,13 +1,13 @@
 import FreeSimpleGUI as sg
+import zipfile_creater
 
 label1 = sg.Text("Select files to compress: ")
 input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Choose")
-
+choose_button1 = sg.FilesBrowse("Choose", key="files")
 
 label2 = sg.Text("Select destination folder: ")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose")
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
 
 compress_button = sg.Button("Compress")
 
@@ -16,5 +16,12 @@ window = sg.Window("File Compressor",
                            [label2, input2, choose_button2],
                            [compress_button]])
 
-window.read()
+while True:
+        event, values = window.read()
+        print(event, values)
+        filepaths = values["files"].split(";")
+        folder = values["folder"]
+        zipfile_creater.make_archive(filepaths, folder)
+        
+        
 window.close()
