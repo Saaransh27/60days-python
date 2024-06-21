e75a56b1d2ec30582a7c0b34f7883026480e2ebe
Saaransh27 <133195191+Saaransh27@users.noreply.github.com>
2024-06-21 15:14:54 +0530

    Add .gitignore file

---
+++ app2-portfolio/Home.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import os
+
+st.set_page_config(layout="wide")
+
+# Define columns for the layout
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Saaransh Jain")
+    content = """
+    Hi, I am Saaransh! I am a Python programmer, a teacher, a college student, and ambitious to become a content creator.
+    I mainly work in Python, JavaScript, and C++ but also have experience working on Figma, Canva, Excel, and Word.
+    I completed schooling in 2022 and would complete my engineering in 2026. My goal is to get into a top IIM for an MBA.
+    """
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.info(content2)
+
+# Define columns for the apps section
+col3, empty_col, col4 = st.columns([1.5, 0.4, 1.5])
+
+# Read the data from the CSV file
+df = pd.read_csv("data.csv", sep=";")
+
+# Function to get image path
+def get_image_path(image_name):
+    return os.path.join("images", image_name)
+
+# Display the first 10 rows of the dataframe in col3
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        image_path = get_image_path(row["image"])
+        if os.path.exists(image_path):
+            st.image(image_path)
+        else:
+            st.warning(f"Image {image_path} not found!")
+        st.write(f"[Source Code]({row['url']})")
+
+# Display the remaining rows of the dataframe in col4
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        image_path = get_image_path(row["image"])
+        st.write(f"Trying to open image at: {image_path}")  # Debug print
+        if os.path.exists(image_path):
+            st.image(image_path)
+        else:
+            st.warning(f"Image {image_path} not found!")
+        st.write(f"[Source Code]({row['url']})")
