88e2f734cdedd97aef24df19dcb24a3d1490580a
Saaransh27 <133195191+Saaransh27@users.noreply.github.com>
2024-06-28 13:40:45 +0530

    added readme

---
+++ app5-excel_to_pdf_invoice/main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+import os
+
+os.makedirs("PDFs", exist_ok=True)
+
+filepaths = glob.glob("invoices/*.xlsx")
+
+for filepath in filepaths:
+       
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    
+    filename = Path(filepath).stem
+    invoice_nr, date = filename.split("-")
+    
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=True)
+    
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=True)
+    
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=25, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=50, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=25, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=50, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=50, h=8, txt=str(row["total_price"]), border=1)
+    
+    pdf.output(f"PDFs/{filename}.pdf")
